0c086ea5511f6a58a6a70c7ece5ca03d3e2e54c8
vis-19maya <165902604+vis-19maya@users.noreply.github.com>
2024-04-20 10:38:40 +0530

    Add files via upload

---
+++ app/src/Pages/AddBlog.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Form from '../Components/Form/Form';
+
+const AddBlog = () => {
+  return (
+    <div>
+    <Form/>
+    
+ </div>
+  );
+}
+export default AddBlog;
---
+++ app/src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Myblog from '../Components/Myblog/Myblog'
+
+ const  Home =() =>{
+  return (
+    <div>
+      <Myblog/>
+    </div>
+  )
+}
+export default Home;
